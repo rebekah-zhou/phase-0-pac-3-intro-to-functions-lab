7ca14bf4102053b0306da08927ca6b0f32f8aa71
Rebekah Zhou <rebekahzhou@gmail.com>
2021-12-16 15:37:41 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // your code here
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+ 
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+
+logShout('Hello');
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+
+logWhisper('Hello');
+
+function sayHiToHeadphonedRoomate(string) {
+    if (string === string.toLowerCase()) {
+        return "I can't hear you!";
+    } else if (string === string.toUpperCase()) {
+        return "YES INDEED!";
+    } else if (string === "Let's have dinner together!") {
+        return "I would love to!";
+    }
+}
